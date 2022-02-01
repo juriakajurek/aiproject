2a8e553bde088523bd104b9384bd7b70f4c896ee
Jarosław Niemiec <48520727+juriakajurek@users.noreply.github.com>
2022-02-02 00:14:00 +0100

    add  login and register views, add emojis checkboxes, add layout and stylings

--- src/utilities.js
+++ src/utilities.js
@@ -1,49 +1,70 @@
-
-
 const fingerJoints = {
-    thumb:[0, 1, 2, 3, 4],
-    indexFinger:[0, 5, 6, 7,8],
-    middleFinger:[0,9,10,11,12],
-    ringFinger:[0,13,14,15,16],
-    pinky:[0,17,18,19,20],
-}
+  thumb: [0, 1, 2, 3, 4],
+  indexFinger: [0, 5, 6, 7, 8],
+  middleFinger: [0, 9, 10, 11, 12],
+  ringFinger: [0, 13, 14, 15, 16],
+  pinky: [0, 17, 18, 19, 20],
+};
+
+export const getEmoji = (emoji) => {
+  switch (emoji) {
+    case 'fingers_splayed':
+      return '🖐️';
+      break;
+    case 'fist':
+      return '✊';
+      break;
+    case 'thumbs_up':
+      return '👍';
+      break;
+    case 'thumbs_down':
+      return '👎';
+      break;
+    case 'victory':
+      return '✌️';
+      break;
+    default:
+      return '';
+      break;
+  }
+};
 
 export const drawHand = (predictions, ctx) => {
-    if(predictions.length>0){
-        predictions.forEach(prediction => {
-            const landmarks = prediction.landmarks;
+  if (predictions.length > 0) {
+    predictions.forEach((prediction) => {
+      const landmarks = prediction.landmarks;
 
-            for (let j=0; j<Object.keys(fingerJoints).length;j++){
-                let finger = Object.keys(fingerJoints)[j];
+      for (let j = 0; j < Object.keys(fingerJoints).length; j++) {
+        let finger = Object.keys(fingerJoints)[j];
 
-                for (let k=0; k<fingerJoints[finger].length-1; k++){
-                    const firstJointIndex = fingerJoints[finger][k];
-                    const secondJointIndex = fingerJoints[finger][k+1];
+        for (let k = 0; k < fingerJoints[finger].length - 1; k++) {
+          const firstJointIndex = fingerJoints[finger][k];
+          const secondJointIndex = fingerJoints[finger][k + 1];
 
-                    ctx.beginPath();
-                    ctx.moveTo(
-                        landmarks[firstJointIndex][0],
-                        landmarks[firstJointIndex][1]
-                    );
-                    ctx.lineTo(
-                        landmarks[secondJointIndex][0],
-                        landmarks[secondJointIndex][1]
-                    );
-                    ctx.strokeStyle = "purple";
-                    ctx.lineWidth = 4;
-                    ctx.stroke();
-                }
-            }
+          ctx.beginPath();
+          ctx.moveTo(
+            landmarks[firstJointIndex][0],
+            landmarks[firstJointIndex][1],
+          );
+          ctx.lineTo(
+            landmarks[secondJointIndex][0],
+            landmarks[secondJointIndex][1],
+          );
+          ctx.strokeStyle = 'purple';
+          ctx.lineWidth = 4;
+          ctx.stroke();
+        }
+      }
 
-            for (let i =0; i<landmarks.length; i++){
-                const x = landmarks[i][0];
-                const y = landmarks[i][1];
+      for (let i = 0; i < landmarks.length; i++) {
+        const x = landmarks[i][0];
+        const y = landmarks[i][1];
 
-                ctx.beginPath();
-                ctx.arc(x,y,3,0,2*Math.PI);
-                ctx.strokeStyle = "red";
-                ctx.stroke();
-            }
-        });
-    }
-}
+        ctx.beginPath();
+        ctx.arc(x, y, 3, 0, 2 * Math.PI);
+        ctx.strokeStyle = 'red';
+        ctx.stroke();
+      }
+    });
+  }
+};
